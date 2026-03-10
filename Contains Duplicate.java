b6ead1cb47171ab4214528249f8d814e5d7e0af2
kavya2005-hub <kavyasakthivelsk@gmail.com>
2026-03-10 21:11:47 +0530

    Update Contains Duplicate.java

--- Contains Duplicate.java
+++ Contains Duplicate.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean hasDuplicate(int[] arr) {
         boolean isduplicate=false;
-        for(int i=0;i<arr.length-1;i++)
+        for(int i=0;i<arr.length;i++)
         {
-            if(arr[i]==arr[i+1])
+            for(int j=i+1;j<arr.length;j++)
+            if(arr[i]==arr[j])
             {
-                isduplicate=true;
+                return true;
             }
         }
         
         
-        return isduplicate;
+        return false;
         
         
     }
