2b2694f7786ea78a928b4c63578e5a91efde6442
kavya2005-hub <kavyasakthivelsk@gmail.com>
2026-03-14 12:25:14 +0530

    Update Contains Duplicate.java

--- Contains Duplicate.java
+++ Contains Duplicate.java
@@ -1,3 +1,16 @@
+/*Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false*/
 class Solution {
     public boolean hasDuplicate(int[] arr) {
         boolean isduplicate=false;
