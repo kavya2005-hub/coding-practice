5effbeb3dd5d379cd3d0a4914f362ffdd79bc462
kavya2005-hub <kavyasakthivelsk@gmail.com>
2026-05-13 20:56:30 +0530

    Update print array in even next odd.java

--- print array in even next odd.java
+++ print array in even next odd.java
@@ -1,1 +1,29 @@
-
+import java.util.Scanner;
+class evenodd
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int size=sc.nextInt();
+int arr[]=new int[size];
+for(int i=0;i<size;i++)
+{
+arr[i]=sc.nextInt();
+}
+for(int i=0;i<size;i++)
+{
+if(arr[i]%2==0)
+{
+System.out.print(arr[i]+" ");
+}
+}
+for(int i=0;i<size;i++)
+{
+if(arr[i]%2!=0)
+{
+System.out.print(arr[i]+" ");
+}
+}
+sc.close();
+}
+}
