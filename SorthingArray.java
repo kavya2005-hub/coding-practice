9675ae3a8d3c2aef46e6a562e64d37bfbf48b27c
kavya s <kavyasakthivelsk@gmail.com>
2026-07-24 21:49:41 +0530

    Initial commit

--- SorthingArray.java
+++ SorthingArray.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class SorthingArray {
-    publ
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr);
+    }
     
 }
