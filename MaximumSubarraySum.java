a49af8170bb580cf0e4f12d16edfee58e83bf72f
kavya s <kavyasakthivelsk@gmail.com>
2026-07-24 21:42:26 +0530

    Initial commit

---
+++ MaximumSubarraySum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MaximumSubarraySum {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        
+        int max=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=i;j<n;j++)
+            {
+                sum+=arr[j];
+            }
+            if(sum>max)
+            {
+                max=sum;
+                
+            }
+        }
+        System.out.println(max);
+    }
+    
+}
