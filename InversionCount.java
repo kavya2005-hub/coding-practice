38e23bc36266397586436f88d095c582926f25f3
kavya s <kavyasakthivelsk@gmail.com>
2026-07-25 21:16:14 +0530

    Initial commit

---
+++ InversionCount.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class InversionCount {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        int sum=0;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    count++;
+                    
+                }
+            }
+        }
+        System.out.println(count);
+    }
+    
+}
