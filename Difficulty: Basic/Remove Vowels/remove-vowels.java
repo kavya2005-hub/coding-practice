55504f66bd75242b81aa295e2c22feb38092c5e6
kavya s <kavyasakthivelsk@gmail.com>
2026-08-04 15:41:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Vowels/remove-vowels.java
@@ -0,0 +1,20 @@
+class Solution {
+    String removeVowels(String s) {
+        StringBuilder sb=new StringBuilder();
+        
+        for(int i= 0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if (c != 'a' && c != 'e' && c != 'i' &&
+                c != 'o' && c != 'u') {
+                    sb.append(c);
+                }
+        }
+                if(sb.length()==0)
+                {
+                    return "";
+                }
+               
+               return sb.toString();
+    }
+}
