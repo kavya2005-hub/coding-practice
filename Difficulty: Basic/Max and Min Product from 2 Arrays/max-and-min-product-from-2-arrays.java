b55903465ffd7ba2c6907c9ebd3d37d7ce6f769f
kavya s <kavyasakthivelsk@gmail.com>
2026-08-05 15:05:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Max and Min Product from 2 Arrays/max-and-min-product-from-2-arrays.java
@@ -0,0 +1,25 @@
+
+class Solution {
+
+    public long minMaxProduct(int[] arr1, int[] arr2) {
+        // code here
+        int max=arr1[0];
+        int min=arr2[0];
+        for(int i=0;i<arr1.length;i++)
+        {
+            if(max<arr1[i])
+            {
+                max=arr1[i];
+            }
+        }
+        for(int i=0;i<arr2.length;i++)
+        {
+            if(min>arr2[i])
+            {
+                min=arr2[i];
+            }
+        }
+        int product=max*min;
+        return product;
+    }
+}
