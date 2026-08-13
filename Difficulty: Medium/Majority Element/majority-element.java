360cfd1a910c27835296341e4cb963a38e30724c
kavya s <kavyasakthivelsk@gmail.com>
2026-08-13 16:35:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+class Solution {
+    int majorityElement(int arr[]) {
+        // code here
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            if(map.get(arr[i])>arr.length/2)
+            {
+                return arr[i];
+            }
+        }
+        return -1;
+        
+    }
+}
